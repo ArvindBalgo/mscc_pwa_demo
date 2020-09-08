6cf5012ac28a3d7678d5e2ae908625c651ef3fd7
Arvind <balgo_arvind@hotmail.com>
2020-09-08 17:27:24 +0400

    'v1'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ApiModel, HeroModel} from './models/heroes-model';
+import {SwUpdate} from '@angular/service-worker';
 
 
 
@@ -12,10 +13,19 @@ import {ApiModel, HeroModel} from './models/heroes-model';
 export class AppComponent implements OnInit{
   title = 'mscc-pwa-demo';
   herosList: Array<HeroModel> = [];
-  constructor(private http$: HttpClient) {
+  constructor(private http$: HttpClient, private swUpdate: SwUpdate) {
   }
 
   ngOnInit(): void {
+    if(this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe((swResponse) => {
+        console.log(swResponse, 'response');
+        if(confirm('New Version is available, Load it?')) {
+          window.location.reload();
+        }
+      })
+    }
+
     this.http$.get('https://www.superheroapi.com/api.php/10223154308827957/search/a').subscribe((response: ApiModel) => {
       console.log(response.results);
       this.herosList = response.results;
